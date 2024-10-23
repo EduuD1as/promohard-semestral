c074acccd89f488577fd8b014e4fa6d12ee54a54
Eduardo Dias Bianchini <edudiasbianchini@gmail.com>
2024-10-22 23:47:33 -0300

    home page creation

---
+++ script/index.js
@@ -0,0 +1,5 @@
+function toggleCategories() {
+    console.log("Botão clicado!"); // Depuração
+    const categoryList = document.getElementById('categoryList');
+    categoryList.classList.toggle('hidden');
+}
